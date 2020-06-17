6826009c1b3c00e41342955ac1fcfbff00a06ef0
mikestowe <mike.stowe@LMRC8714.local>
2020-06-16 20:55:02 -0700

    changing php

--- rng-sms-php/courseBase.sh
+++ rng-sms-php/courseBase.sh
@@ -1,5 +1,9 @@
 apt install apache2 -y
-apt instal php -y
+
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:ondrej/php
+sudo apt install php7.3 php7.3-common php7.3-opcache php7.3-cli php7.3-gd php7.3-curl php7.3-mysql
+
 service apache2 start
 cd /var/www/html
 rm index.html
