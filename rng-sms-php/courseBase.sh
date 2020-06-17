46e4bf343e26320804720ad9ff6795ea8f1b21be
mikestowe <mike.stowe@LMRC8714.local>
2020-06-16 20:57:01 -0700

    removed composer

--- rng-sms-php/courseBase.sh
+++ rng-sms-php/courseBase.sh
@@ -7,7 +7,3 @@ sudo apt install php7.3 php7.3-common php7.3-opcache php7.3-cli php7.3-gd php7.3
 service apache2 start
 cd /var/www/html
 rm index.html
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === 'e0012edf3e80b6978849f5eff0d4b4e4c79ff1609dd1e613307e16318854d24ae64f26d17af3ef0bf7cfb710ca74755a') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
